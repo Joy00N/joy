681a54b5c23d41c5b4a27b03ce24042202429689
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-04-10 01:29:49 -0400

    mongo setup

--- src/main/java/com/example/joy/JoyApplication.java
+++ src/main/java/com/example/joy/JoyApplication.java
@@ -1,13 +1,47 @@
 package com.example.joy;
 
+import com.example.joy.model.Customer;
+import com.example.joy.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-public class JoyApplication {
+public class JoyApplication implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JoyApplication.class, args);
 	}
 
+	@Override
+	public void run(String ...args) throws Exception{
+		repository.deleteAll();
+
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+	}
 }
