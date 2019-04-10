de3fceed88600ec2d0fe35bcd1cde1ebf87d1758
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-04-10 01:29:49 -0400

    mongo setup

--- src/main/java/com/example/joy/model/Customer.java
+++ src/main/java/com/example/joy/model/Customer.java
@@ -3,7 +3,6 @@ package com.example.joy.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.math.BigInteger;
 
 @Document(collection = "customers")
