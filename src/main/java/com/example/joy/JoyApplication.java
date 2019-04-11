bed2370756b9ac568f1e51f6c5c9408068f8cd8e
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-04-10 23:42:22 -0400

    mongo & rest api setup

--- src/main/java/com/example/joy/JoyApplication.java
+++ src/main/java/com/example/joy/JoyApplication.java
@@ -10,38 +10,38 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-public class JoyApplication implements CommandLineRunner {
+public class JoyApplication{
+//
+//    @Autowired
+//    private CustomerRepository repository;
 
-	@Autowired
-	private CustomerRepository repository;
+    public static void main(String[] args) {
+        SpringApplication.run(JoyApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JoyApplication.class, args);
-	}
-
-	@Override
-	public void run(String ...args) throws Exception{
-		repository.deleteAll();
-
-		repository.save(new Customer("Alice", "Smith"));
-		repository.save(new Customer("Bob", "Smith"));
-
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
-
-		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("Alice"));
-
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Smith")) {
-			System.out.println(customer);
-		}
-	}
+//    @Override
+//    public void run(String... args) throws Exception {
+//        repository.deleteAll();
+//
+//        repository.save(new Customer("Alice", "Smith"));
+//        repository.save(new Customer("Bob", "Smith"));
+//
+//        System.out.println("Customers found with findAll():");
+//        System.out.println("-------------------------------");
+//        for (Customer customer : repository.findAll()) {
+//            System.out.println(customer);
+//        }
+//        System.out.println();
+//
+//        // fetch an individual customer
+//        System.out.println("Customer found with findByFirstName('Alice'):");
+//        System.out.println("--------------------------------");
+//        System.out.println(repository.findByFirstName("Alice"));
+//
+//        System.out.println("Customers found with findByLastName('Smith'):");
+//        System.out.println("--------------------------------");
+//        for (Customer customer : repository.findByLastName("Smith")) {
+//            System.out.println(customer);
+//        }
+//    }
 }
--- src/main/java/com/example/joy/repository/CustomerRepository.java
+++ src/main/java/com/example/joy/repository/CustomerRepository.java
@@ -2,10 +2,12 @@ package com.example.joy.repository;
 
 import com.example.joy.model.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "customers", path = "/customers")
 public interface CustomerRepository extends MongoRepository<Customer, String> {
-    public Customer findByFirstName(String firstName);
-    public List<Customer> findByLastName(String lastName);
+    Customer findByFirstName(String firstName);
+    List<Customer> findByLastName(String lastName);
 }
