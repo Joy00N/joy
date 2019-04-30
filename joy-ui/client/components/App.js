621d455c83d64f603e6a6212df4f80d1fd419fe2
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-04-29 22:39:18 -0400

    commit

---
+++ joy-ui/client/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() extends Component {
+    render(){
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Hello Yoonhee</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
