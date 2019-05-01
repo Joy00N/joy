f8b2a990bf11e3be86ba20305d7ef37051254a02
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-05-01 08:34:03 -0400

    webpack configuration done!

--- joy-ui/webpack.config.js
+++ joy-ui/webpack.config.js
@@ -7,14 +7,12 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /.jsx?$/,
-            loader: 'babel-loader',
             exclude: /node_modules/,
-            query: {
-                presets: ['es2015', 'react']
-            }
-        },
+            use:{
+                loader: 'babel-loader'
+            }},
             {
                 test: /\.css$/,
                 loader: "style-loader!css-loader"
