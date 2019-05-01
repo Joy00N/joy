fbe5b22c11f0f6755c1148db3b2c9b0fad80e8d3
Yoonhee <yoonhee.jo.cs@gmail.com>
2019-05-01 08:34:03 -0400

    webpack configuration done!

--- joy-ui/client/components/App.js
+++ joy-ui/client/components/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
-
-function App() extends Component {
-    render(){
+class App extends Component {
+    render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-title">Hello Yoonhee</h1>
+                    <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
             </div>
         );
     }
 }
-
 export default App;
